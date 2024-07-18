40fb9eddb38733d8c6ce0f6063fbd4ffd5de2cfb
Bajrangbana <70948330+Bajrangbana@users.noreply.github.com>
2024-07-18 08:27:16 +0530

    Update commentController.js
    fixed comment reply issue

--- backend/controller/commentController.js
+++ backend/controller/commentController.js
@@ -1,63 +1,88 @@
 const Comment = require("../models/commentModel");
 
 const postComment = async (req, res) => {
-  const { username, text, parentId } = req.body;
-  const comment = new Comment({ username, text, parentId });
+  try {
+    const { username, text, parentId } = req.body;
+    const comment = new Comment({ username, text, parentId });
 
-  await comment.save();
+    await comment.save();
 
-  if (parentId) {
-    const parentComment = await Comment.findById(parentId);
-    parentComment.children.push(comment._id);
-    await parentComment.save();
+    if (parentId) {
+      const parentComment = await Comment.findById(parentId);
+      if (!parentComment) {
+        return res.status(404).send({ message: "Parent comment not found" });
+      }
+      parentComment.children.push(comment._id);
+      await parentComment.save();
+    }
+
+    res.status(201).send(comment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal server error" });
   }
-
-  res.status(201).send(comment);
 };
 
 const getComments = async (req, res) => {
-  const populateChildren = async (comment) => {
-    await comment
-      .populate({
-        path: "children",
-        populate: {
-          path: "children",
-          model: "Comment",
-          populate: {
-            path: "children",
-            model: "Comment",
-          },
-        },
-      })
-      .execPopulate();
-  };
+  try {
+    const populateChildren = async (comment) => {
+      await comment.populate("children");
+      if (comment.children.length > 0) {
+        for (let child of comment.children) {
+          await populateChildren(child);
+        }
+      }
+    };
 
-  const comments = await Comment.find({ parentId: null }).populate({
-    path: "children",
-    populate: {
-      path: "children",
-      model: "Comment",
-      populate: {
-        path: "children",
-        model: "Comment",
-      },
-    },
-  });
+    const populateAllChildren = async (comments) => {
+      for (let comment of comments) {
+        await populateChildren(comment);
+      }
+    };
 
-  res.status(200).send(comments);
+    const getCommentsWithAllChildren = async () => {
+      let comments = await Comment.find({ parentId: null }).exec();
+      await populateAllChildren(comments);
+      return comments;
+    };
+
+    const comments = await getCommentsWithAllChildren();
+
+    res.status(200).send(comments);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal server error" });
+  }
 };
 
 const updateComment = async (req, res) => {
-  const { id } = req.params;
-  const { text } = req.body;
-  const comment = await Comment.findByIdAndUpdate(id, { text }, { new: true });
-  res.status(200).send(comment);
+  try {
+    const { id } = req.params;
+    const { text } = req.body;
+    const comment = await Comment.findByIdAndUpdate(
+      id,
+      { text },
+      { new: true }
+    );
+    if (!comment) {
+      return res.status(404).send({ message: "Comment not found" });
+    }
+    res.status(200).send(comment);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal server error" });
+  }
 };
 
 const deleteComment = async (req, res) => {
-  const { id } = req.params;
-  await Comment.findByIdAndDelete(id);
-  res.status(204).send();
+  try {
+    const { id } = req.params;
+    await Comment.findByIdAndDelete(id);
+    res.status(204).send();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal server error" });
+  }
 };
 
 module.exports = {
